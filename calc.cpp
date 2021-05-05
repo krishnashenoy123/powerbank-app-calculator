4148a8bc4ddfa25b5f3b83ecf03c7b814f527b55
krishnashenoy123 <57287781+krishnashenoy123@users.noreply.github.com>
2021-05-05 21:44:23 +0530

    removed the for loop and made the code more efficient
    added the compound interest formula

--- calc.cpp
+++ calc.cpp
@@ -1,15 +1,11 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-
-int process(int amount, float interest)
-{
-    amount += amount*interest;
-    return amount;
-}
 
 int main()
 {
-    int plan, amount, times;
+    int plan;
+    double amount, times, total;
     float interest;
     cout << "Enter plan: 5/10/35/120/200-- ";
     cin >> plan;
@@ -30,10 +26,8 @@ int main()
     cin >> amount;
     cout << "Enter the times that you want to keep: ";
     cin >> times;
-    for(int i=0; i<times; i++)
-    {
-        amount = process(amount, interest); 
-    }
-    cout << endl << "After " << plan*times << " days: " << endl << amount;
+    
+    total = amount*(pow((1+interest),times));
+    cout << endl << "After " << plan*times << " days: " << endl << total;
     return 0;
 }
